3ec9e25b4dcbe9064b2c66f1a0f6e5295a249d0b
Mr. Fleming <efleming@stpatrick.org>
2017-02-01 10:38:16 -0600

    uploaded the degree material

---
+++ degree.js
@@ -0,0 +1,23 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+
+}
+
+function degreecentrality(){
+
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
